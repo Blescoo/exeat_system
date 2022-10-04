86bd93855186c909a98962b2e89954dbb0e1da8c
Arubiewe <arubiewe@gmail.com>
2022-10-04 11:59:31 +0100

    Updated the logout destroy session

--- auth/admin_page.php
+++ auth/admin_page.php
@@ -37,7 +37,7 @@ session_start();
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="" class="mr-2" alt=""/>Crowford Exeat</a>
+        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="" class="mr-2" alt=""/>Crawford Exeat</a>
         <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
